33cfe7e13d9c0f9837c46c769db70219fd0fe409
Danil Pismenny <danil@orionet.ru>
2011-11-05 23:18:09 +0400

    unicorn update

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -26,8 +26,8 @@ if rails_env=='production'
   worker_processes 4
 
   APP_PATH = '/home/wwwdata/chebytoday.ru/'
-  listen 4005, :tcp_nopush => true
-  #listen APP_PATH + "shared/tmp/unicorn.sock", :backlog => 64
+  #listen 4005, :tcp_nopush => true
+  listen APP_PATH + "shared/tmp/unicorn.sock", :backlog => 64
 
   working_directory APP_PATH + "current"
   pid APP_PATH + "shared/pids/unicorn.pid"

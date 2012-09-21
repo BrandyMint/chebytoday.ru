e62700794df537dcff13e925e2cc1f5862bb319c
Alexey Illarionov <littlesavage@rambler.ru>
2012-09-21 16:16:49 +0400

    s/icfdev/brandymint/

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,7 @@
 if defined? Airbrake
    Airbrake.configure do |config|
       config.api_key   = '9aa668deae35de618a056e7801da5329'
-      config.host   = 'errbit.icfdev.ru'
+      config.host   = 'errbit.brandymint.ru'
       config.port   = 80
       config.secure = config.port == 443
    end

786e8c87f0fe0702f67715ec85007a9db9c8cf43
Nikolaev Artem N <nuriko@inbox.ru>
2013-01-22 13:11:03 +0400

    Feature: обновление рельсов на chebytoday ru

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,8 @@ Chebytoday::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
+  # TODO Поправить в связи с новыми рельсами
+  #config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -1,6 +1,5 @@
 raw_config = File.read("#{Rails.root}/config/app_config.yml")
 APP_CONFIG = YAML.load(raw_config)[Rails.env].symbolize_keys
-
-MAIL_CONFIG = APP_CONFIG[:mail].symbolize_keys
+MAIL_CONFIG = APP_CONFIG[:mail]
 
 ActionMailer::Base.smtp_settings = MAIL_CONFIG
